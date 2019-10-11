654a01572624f4ae7ee67597d467cc68ffb54dff
Christine Stanley <chris@earlswoodmarketing.com>
2019-10-11 15:03:26 +0100

    Day 18: 3 challenges complete

--- Project1-WeSplit/WeSplit/ContentView.swift
+++ Project1-WeSplit/WeSplit/ContentView.swift
@@ -10,19 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    //@State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    let tipEmojis = ["😏","🙂","😀","😋","😢"]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var peopleCount: Double {
+        return Double(Int(numberOfPeople) ?? 0) + 2
+    }
+    var grandTotal: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         let tipValue = orderAmount / 100 * tipSelection
-        let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-        return amountPerPerson
+        return orderAmount + tipValue
+    }
+    var totalPerPerson: Double {
+        return grandTotal / peopleCount
     }
     
     var body: some View {
@@ -32,21 +37,29 @@ struct ContentView: View {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
-                            Text("\($0) people")
-                        }
-                    }
+                    // Day 18 Challenge 3: Change the “Number of people” picker to be a text field, making sure to use the correct keyboard type.
+                    //Picker("Number of people", selection: $numberOfPeople) {
+                    //    ForEach(2..<100) {
+                    //        Text("\($0) people")
+                    //    }
+                    //}
+                    TextField("Number of People", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
                 }
-                Section(header: Text("How much tip do you want to leave?")) {
+                Section(header: Text("How much tip do \(tipEmojis[tipPercentage]) want to leave? ")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<tipPercentages.count ) {
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 }
-                Section {
-                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                // Day 18 Challenge 2: Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people.
+                Section(header: Text("Total including tip")) {
+                    Text("£\(grandTotal, specifier: "%.2f")")
+                }
+                // Day 18 Challenge 1: Add a header to the third section, saying “Amount per person”
+                Section(header: Text("Amount per person")) {
+                    Text("£\(totalPerPerson, specifier: "%.2f")")
                 }
             }
             .navigationBarTitle("WeSplit")
