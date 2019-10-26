feab1c160bfc62bb6f65223631ba09ca327cde0e
Christine Stanley <chris@earlswoodmarketing.com>
2019-10-26 20:02:11 +0100

    Day 33 complete

--- Project6-Animations/Animations/ContentView.swift
+++ Project6-Animations/Animations/ContentView.swift
@@ -8,71 +8,164 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
     @State private var stepAnimationAmount: CGFloat = 1
     @State private var rotateAnimationAmount = 0.0
     
+    @State private var enabled = false
+    
+    let letters = Array("Hello SwiftUI")
+    @State private var dragAmount = CGSize.zero
+
+    @State private var isShowingRed = false
+    
     var body: some View {
-        VStack {
-            
-            // Implicit animation
-            // Use padding on the Text rather than the button so the whole circle it tappable
-            Button(action: {
-                //self.animationAmount += 1
-            }) {
-                Text("Tap Me")
-                    .padding(50)
-            }
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(Color.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(Double(2 - animationAmount))
-                    .animation(
-                        Animation.easeInOut(duration: 1)
-                            .repeatForever(autoreverses: false)
+        TabView {
+            VStack {
+
+                // Implicit animation
+                // Use padding on the Text rather than the button so the whole circle it tappable
+                Button(action: {
+                    //self.animationAmount += 1
+                }) {
+                    Text("Tap Me")
+                        .padding(50)
+                }
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color.red)
+                        .scaleEffect(animationAmount)
+                        .opacity(Double(2 - animationAmount))
+                        .animation(
+                            Animation.easeInOut(duration: 1)
+                                .repeatForever(autoreverses: false)
+                    )
                 )
-            )
-                //.blur(radius: (animationAmount - 1) * 3)
-                .onAppear {
-                    self.animationAmount = 2
+                    //.blur(radius: (animationAmount - 1) * 3)
+                    .onAppear {
+                        self.animationAmount = 2
+                }
+
+                Divider()
+
+                // Animating bindings
+                Stepper("Scale amount: \(stepAnimationAmount, specifier: "%g" )", value: $stepAnimationAmount.animation(), in: 1...10)
+
+                Spacer()
+
+                Button("Tap Me") {
+                    self.stepAnimationAmount += 1
+                }
+                .padding(40)
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(stepAnimationAmount)
+
+                Spacer()
+
+                Divider()
+
+                // Explicit animations
+                Button("Tap Me") {
+                    withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                        self.rotateAnimationAmount += 360
+                    }
+                }
+                .padding(50)
+                .background(Color.green)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .rotation3DEffect(.degrees(rotateAnimationAmount), axis: (x: 0, y: 1, z: 0))
             }
-            
-            Divider()
-            
-            // Animating bindings
-            Stepper("Scale amount: \(stepAnimationAmount, specifier: "%g" )", value: $stepAnimationAmount.animation(), in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap Me") {
-                self.stepAnimationAmount += 1
+            .tabItem {
+                Image(systemName: "1.circle.fill")
+                Text("")
             }
-            .padding(40)
-            .background(Color.orange)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(stepAnimationAmount)
-            
-            Spacer()
-            
-            Divider()
-            
-            // Explicit animations
-            Button("Tap Me") {
-                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                    self.rotateAnimationAmount += 360
+            VStack {
+                Button("Tap Me") {
+                    self.enabled.toggle()
+                }
+                .frame(width: 200, height: 200)
+                .background(enabled ? Color.blue : Color.red)
+                .animation(.default)
+                // .animation(nil)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+                .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+            }
+            .tabItem {
+                Image(systemName: "2.circle.fill")
+                Text("")
+            }
+
+            VStack {
+                HStack(spacing: 0) {
+                    ForEach(0..<letters.count) { num in
+                        Text(String(self.letters[num]))
+                            .padding(5)
+                            .font(.title)
+                            .background(self.enabled ? Color.blue : Color.red)
+                            .offset(self.dragAmount)
+                            .animation(Animation.default.delay(Double(num) / 20))
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { self.dragAmount = $0.translation }
+                        .onEnded { _ in
+                            self.dragAmount = .zero
+                            self.enabled.toggle()
+                        }
+                )
+            }
+            .tabItem {
+                Image(systemName: "3.circle.fill")
+                Text("")
+            }
+
+            VStack {
+                Button("Tap Me") {
+                    withAnimation {
+                        self.isShowingRed.toggle()
+                    }
+                }
+                
+                if isShowingRed {
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: 200, height: 200)
+                        // .transition(.scale)
+                        // .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                        .transition(.pivot)
                 }
             }
-            .padding(50)
-            .background(Color.green)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .rotation3DEffect(.degrees(rotateAnimationAmount), axis: (x: 0, y: 1, z: 0))
+            .tabItem {
+                Image(systemName: "4.circle.fill")
+                Text("")
+            }
         }
     }
 }
---
+++ Project6-Animations/README.md
@@ -0,0 +1,24 @@
+# 100DaysOfSwiftUI
+## Project 6: Animations - days 32 to 34
+In this technique project we’re going to look at a range of animations and transitions with SwiftUI. Some are easy – in fact, you’ll be able to get great results almost immediately! – but some require more thinking. All will be useful, though, particularly as you work to make sure your apps are attractive and help guide the users eyes as best as you can.
+
+Day 32 covers:
+- Implicit animations
+- Customising animations
+- Animating bindings
+- Creating explicit animations
+
+Days 33 covers some of the more advanced techniques including:
+- Controlling the animation stack
+- Animating gestures
+- Showing and hiding view transitions
+- Building custom transitions using ViewModifier
+
+## Challenges
+Go back to the Guess the Flag project 2 and add some animation:
+
+1. When you tap the correct flag, make it spin around 360 degrees on the Y axis.
+
+2. Make the other two buttons fade out to 25% opacity.
+
+3. And if you tap on the wrong flag? Well, that’s down to you – get creative!
