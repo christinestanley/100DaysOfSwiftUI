bb94a1528fa03fc4655ec8531a2cae388b7dfc47
Christine Stanley <chris@earlswoodmarketing.com>
2019-11-09 14:28:46 +0000

    Day 44 complete

--- Project9-Drawing/Drawing/ContentView.swift
+++ Project9-Drawing/Drawing/ContentView.swift
@@ -8,9 +8,161 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    var insetAmount: CGFloat = 0
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+
+    // How wide to make each petal
+    var petalWidth: Double = 100
+
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+
+        // now send the main path back
+        return path
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    //.strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        // Draw using Metal
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        TabView {
+            // Day 43
+            VStack {
+                Path { path in
+                    path.move(to: CGPoint(x: 200, y: 100))
+                    path.addLine(to: CGPoint(x: 100, y: 300))
+                    path.addLine(to: CGPoint(x: 300, y: 300))
+                    path.addLine(to: CGPoint(x: 200, y: 100))
+                    path.addLine(to: CGPoint(x: 100, y: 300))
+                }
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                
+                Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .stroke(Color.blue, lineWidth: 10)
+                .frame(width: 300, height: 300)
+            }
+            .tabItem {
+                Image(systemName: "1.circle.fill")
+                Text("")
+            }
+            
+            // Day 44
+            VStack {
+                Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                    .fill(Color.red, style: FillStyle(eoFill: true))
+                
+                Text("Offset")
+                Slider(value: $petalOffset, in: -40...40)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Width")
+                Slider(value: $petalWidth, in: 0...100)
+                    .padding(.horizontal)
+            }
+            .tabItem {
+                Image(systemName: "2.circle.fill")
+                Text("")
+            }
+            VStack {
+                Capsule()
+                    .strokeBorder(ImagePaint(image: Image("UK"), scale: 0.5), lineWidth: 20)
+                .frame(width: 300, height: 200)
+            }
+            .tabItem {
+                Image(systemName: "3.circle.fill")
+                Text("")
+            }
+            VStack {
+                ColorCyclingCircle(amount: self.colorCycle)
+                    .frame(width: 300, height: 300)
+
+                Slider(value: $colorCycle)
+            }
+            .tabItem {
+                Image(systemName: "4.circle.fill")
+                Text("")
+            }
+        }
     }
 }
 
--- Project9-Drawing/README.md
+++ Project9-Drawing/README.md
@@ -1,4 +1,7 @@
 # 100DaysOfSwiftUI
 ## Project 9: Drawing - day 43, 44, 45 and 46
+In this technique project we’re going to take a close look at drawing in SwiftUI, including creating custom paths and shapes, animating your changes, solving performance problems, and more – it’s a really big topic, and deserves close attention.
+
+Behind the scenes, SwiftUI uses the same drawing system that we have on the rest of Apple’s frameworks: Core Animation and Metal. Most of the time Core Animation is responsible for our drawing, whether that’s custom paths and shapes or UI elements such as `TextField`, but when things really get complex we can move down to Metal – Apple’s low-level framework that’s optimized for complex drawing. One of the neat features of SwiftUI is that these two are almost interchangeable: we can move from Core Animation to Metal with one small change.
 
 ## Challenges
