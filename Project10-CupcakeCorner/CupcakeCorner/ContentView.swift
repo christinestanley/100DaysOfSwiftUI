23d38e62536fe7135a97178fe316731dd9aa0697
Christine Stanley <chris@earlswoodmarketing.com>
2019-11-11 10:44:08 +0000

    Day 49 demo code complete

--- Project10-CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Project10-CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,9 +8,93 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+       case name
+    }
+    
+    @Published var name = "Paul Hudson"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State var results = [Result]()
+    
+    @State var username = ""
+    @State var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Form {
+                Section {
+                    TextField("Username", text: $username)
+                    TextField("Email", text: $email)
+                }
+
+                Section {
+                    Button("Create account") {
+                        print("Creating account…")
+                    }
+                }
+                .disabled(disableForm)
+            }
+            
+            
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }
+            .onAppear(perform: loadData)
+        }
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+
+        let request = URLRequest(url: url)
+        
+        // Don't forget resume() or it won't run
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            
+        }.resume()
     }
 }
 
