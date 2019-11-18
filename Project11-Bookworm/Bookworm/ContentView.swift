d711c430f6924c9dce92daca35cfab0fda45f97c
Christine Stanley <chris@earlswoodmarketing.com>
2019-11-18 18:43:44 +0000

    Day 54 complete

--- Project11-Bookworm/Bookworm/ContentView.swift
+++ Project11-Bookworm/Bookworm/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                ForEach(books, id:\.self) { book in
+                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddScreen.toggle()
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+            }
+        }
     }
 }
 
