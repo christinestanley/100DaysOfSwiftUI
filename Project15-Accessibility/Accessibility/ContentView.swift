ae8ce081b85130ec66a4bf63ad80145bbe4c2838
Christine Stanley <chris@earlswoodmarketing.com>
2019-12-10 18:00:02 +0000

    Day 74 complete

--- Project15-Accessibility/Accessibility/ContentView.swift
+++ Project15-Accessibility/Accessibility/ContentView.swift
@@ -9,8 +9,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var estimate = 25.0
+    @State private var rating = 3
+    
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 10) {
+            Image(pictures[selectedPicture])
+                .resizable()
+                .frame(width: 150, height: 100)
+                .accessibility(label: Text(labels[selectedPicture]))
+                .accessibility(addTraits: .isButton)
+                .accessibility(removeTraits: .isImage)
+                .onTapGesture {
+                    self.selectedPicture = Int.random(in: 0...3)
+            }
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .combine)
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibility(label: Text("Your score is 1000"))
+            
+            // Without accessibilty modifier gives value as %
+            Slider(value: $estimate, in: 0...50)
+            .padding()
+            .accessibility(value: Text("\(Int(estimate))"))
+            
+            // In iOS13.2 has no default voice over value
+            Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+            .accessibility(value: Text("\(rating) out of 5"))
+        }
+        .padding(20)
     }
 }
 
