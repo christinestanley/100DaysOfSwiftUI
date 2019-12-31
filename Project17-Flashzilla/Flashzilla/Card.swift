09df32ad71e985b740db4639cadb4b6bccd18c24
Christine Stanley <chris@earlswoodmarketing.com>
2019-12-31 19:50:35 +0000

    Day 88 complete

---
+++ Project17-Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Chris on 31/12/2019.
+//  Copyright © 2019 Earlswood Marketiing Ltd. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static var example: Card {
+        return Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
--- Project17-Flashzilla/Flashzilla/ContentView.swift
+++ Project17-Flashzilla/Flashzilla/ContentView.swift
@@ -9,13 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id:\.self) { index in
+                        CardView(card: self.cards[index]) {
+                            withAnimation {
+                                self.removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
     }
 }
